2fafaf206d83eaf2249fed9957dcbebabdcc43cd
mrkvon <michal.salajka@email.cz>
2015-05-25 20:20:06 +0200

    messing with signup: mongodb and data validation

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -9,7 +9,7 @@ router.get('/', function (req, res, next) {
     var sess = req.session;
 
     if(sess.logged === true){
-        res.end('you are already logged in as ' + sess.username); //TODO
+        res.render('sysinfo', {msg: 'you are already logged in as ' + sess.username}); //TODO
     }
     else {
         res.render('login');
@@ -19,7 +19,7 @@ router.get('/', function (req, res, next) {
 router.post('/', function (req, res, next) {
     var sess = req.session;
     if(sess.logged === true){
-        res.end('you are already logged in as ' + sess.username); //TODO
+        res.render('sysinfo', {msg: 'you are already logged in as ' + sess.username}); //TODO
     }
     else{
         //TODO first some simple validation of values
@@ -32,13 +32,15 @@ router.post('/', function (req, res, next) {
                 if (data.logged === true) {
                     sess.logged = true;
                     sess.username = req.body.username;
-                    res.end('login successful');
+                    res.render('sysinfo', {msg: 'login successful'}); //TODO
                 }
                 else {
-                    res.end('login not successful');
+                    res.render('sysinfo', {msg: 'login not successful'}); //TODO
                 }
             },
-            function (reason) {res.end('problem');}
+            function (reason) {
+                res.render('sysinfo', {msg: 'Error: ' + reason}); //TODO
+            }
         );
         
     }
@@ -46,22 +48,18 @@ router.post('/', function (req, res, next) {
 });
 
 function authenticate (username, password) {
-    console.log('authentication');
-    console.log(username, password);
     var deferred = Q.defer();
     setTimeout(function(){
-        console.log('timeout ended')
         var logged = false;
         if(username == 'michal' && password == 'secret'){
             logged = true;
         }
-        console.log(logged);
         if(false) deferred.reject();
         else{
-            console.log('resolved');
             deferred.resolve({logged:logged, username: username});
         }
     },1000);
     return deferred.promise;
 };
+
 module.exports=router;
--- app/controllers/signup.js
+++ app/controllers/signup.js
@@ -2,18 +2,144 @@
 
 var express = require('express');
 var router = express.Router();
+var Q = require('q');
+var User = require('../models/user');
 
 //var mongoose = require('mongoose');
 //var User = require('../models/user');
 
 router.get('/', function(req, res, next){
-    res.render('signup');
+    var sess = req.session;
+    if(sess.logged === true){
+        res.render('sysinfo', {msg: 'you are logged in as ' + sess.username + '. To sign up you need to log out first.'});
+    }
+    else {
+        res.render('signup');
+    }
 });
 
 router.post('/', function(req, res, next){
-    console.log(req.body);
-    res.end(JSON.stringify(req.body));
+    var form = req.body;
+    var formData = {
+        name: form.name,
+        surname: form.surname,
+        username: form.username,
+        email: form.email,
+        password: form.password,
+        password2: form.password2
+    };
+
+    //first let's validate data from the form
+                            
+    validate(formData)
+    .then(function (validateData) {
+        if(validateData.valid === true){
+            var newUser = new User({
+                username: formData.username,
+                email: formData.email,
+                profile: {
+                    name: formData.name,
+                    surname: formData.surname
+                },
+                login: {
+                    password: formData.password //(TODO hash to be secure!!!)
+                }
+            });
+
+            newUser.save(function(err, nu) {
+                if (err) return console.error(err);
+                res.end(JSON.stringify(nu));
+            });
+        }
+        else{
+            res.end(JSON.stringify(validateData.errors));
+        }
+    });  
 });
 
 module.exports = router;
 
+function validate(formData) {
+    //things to validate:   username regex
+    //                      username unique
+    //                      email regex
+    //                      email unique
+    //                      name, surname regex
+    //                      password regex
+    //                      passwords match
+    
+    var deferred = Q.defer(),
+        valid = true,
+        errors = {};
+
+    //username regex
+    var usernameRegex = /^([a-z0-9_\-\.]{2,32})$/;
+    if(usernameRegex.test(formData.username) === false){
+        valid = false;
+        errors.username = errors.username || [];
+        errors.username.push('wrong username format (TODO)');
+    }
+
+    //email regex
+    var emailRegex = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    if(emailRegex.test(formData.email) === false){
+        valid = false;
+        errors.email = errors.email || [];
+        errors.email.push('invalid email');
+    }
+    
+    //name, surname max 128 characters long
+    if(formData.name.length > 128){
+        valid = false;
+        errors.name = errors.name || [];
+        errors.name.push('name can be max 128 characters long');
+    }
+
+    if(formData.surname.length > 128){
+        valid = false;
+        errors.surname = errors.surname || [];
+        errors.surname.push('surname can be max 128 characters long');
+    }
+
+    //password regex
+    var passwordRegex = /(?=^.{8,128}$)(?=.*[a-zA-Z0-9])(?=.*[^A-Za-z0-9]).*$/
+    if(passwordRegex.test(formData.password) === false){
+        valid = false;
+        errors.password = errors.password || [];
+        errors.password.push('password must be at least 8 characters long and contain [a-zA-Z0-9] and some special character');
+    }
+
+    //password match
+    if(formData.password !== formData.password2){
+        valid = false;
+        errors.password2 = errors.password2 || [];
+        errors.password2.push('passwords don\'t match');
+    }
+
+    //asynchronously check if username is unique
+    User.find({ username: formData.username })
+    .exec()
+    .then(function(userArray){
+        console.log(userArray);
+        if(userArray.length > 0){
+            valid = false;
+            errors.username = errors.username || [];
+            errors.username.push('username must be unique');
+        }
+    })
+    .then(function(){
+        //asynchronously check if email is unique
+        User.find({ email: formData.email })
+        .exec()
+        .then(function (userArray) {
+            if(userArray.length > 0) {
+                valid = false;
+                errors.email = errors.email || [];
+                errors.email.push('email must be unique');
+            }
+            deferred.resolve({valid: valid, errors: errors});
+        });
+    });
+
+    return deferred.promise;
+}
