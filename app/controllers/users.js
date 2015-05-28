e755dba6f330382d7d60f5724c725a33b48ec491
mrkvon <michal.salajka@email.cz>
2015-05-29 00:38:05 +0200

    messing with user profile validation (unfinished)

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -21,7 +21,7 @@ router.get('/:username', function (req, res, next) {
     Q.when(UserModel.find({username: username}).exec())
     .then(function (userArray) {
         console.log(userArray.length);
-        if(userArray.length == 0) {
+        if(userArray.length === 0) {
             res.end('user '+ username +' was not found or is hidden');
             throw new Error('abort promise chain');
         }
@@ -84,8 +84,7 @@ router.post('/:username/edit', function (req, res, next){
                         surname: profileData.surname,
                         about: profileData.about
                     }
-                },
-                options = { multi: true };
+                };
 
             UserModel.update(conditions, update, {}, function(err, affected){
                 console.log(affected);
@@ -104,7 +103,42 @@ router.post('/:username/edit', function (req, res, next){
 var validateProfile = function (profileData) {
     var deferred = Q.defer();
     process.nextTick(function () {
-        deferred.resolve({valid: true, errors: {}});
+        var valid = true;
+        var errors = {};
+        //validate name (0 - 256 characters)
+        if (profileData.name.length > 256) {
+            valid = false;
+            errors.name = errors.name || [];
+            errors.name.push('name can be max 256 characters long');
+        }
+        //validate surname (0 - 256 characters)
+        if (profileData.surname.length > 256) {
+            valid = false;
+            errors.surname = errors.surname || [];
+            errors.surname.push('surname can be max 256 characters long');
+        }
+        //validate gender (unspecified, male, female, other)
+        var genderArray = ['unspecified', 'male', 'female', 'other'];
+        if (!(genderArray.indexOf(profileData.gender) > -1)) {
+            valid = false;
+            errors.gender = errors.gender || [];
+            errors.gender.push('please select gender from the list provided');
+        }
+        //validate birthday
+        var birthdayRegex = /^(19|20)\d\d[\-\/.](0[1-9]|1[012])[\-\/.](0[1-9]|[12][0-9]|3[01])$/;
+        if (!profileData.birthday.match(birthdayRegex) && profileData.birthday !== '') {
+            valid = false;
+            errors.birthday = errors.birthday || [];
+            errors.birthday.push('birthday is in wrong format. please use yyyy-mm-dd');
+        }
+        //validate about (0 - 16384 characters)
+        if (profileData.about.length > 16384) {
+            valid = false;
+            errors.about = errors.about || [];
+            errors.about.push('description is too long (max 16384 characters)');
+        }
+
+        deferred.resolve({valid: valid, errors: errors});
     });
     
     return deferred.promise;
@@ -115,13 +149,13 @@ var makeProfileData = function (userData) {
     process.nextTick(function(){
         var profile = {};
         //age
-        profile.age = userData.profile.birthday_v === true ? '' + countAge(userData.profile.birthday) + ' years old' : null ;
+        profile.age = userData.profile.birthday != '' ? '' + countAge(userData.profile.birthday) + ' years old' : null ;
         //gender
-        profile.gender = userData.profile.gender_v === true ? userData.profile.gender : null;
+        profile.gender = userData.profile.gender != '' ? userData.profile.gender : '';
         //joined
         profile.joined = userData.account.join_date;
         //last login
-        profile.lastLogin = '30 minutes ago';
+        profile.lastLogin = countLastLogin(userData.account.last_login);
         //name
         profile.name = userData.profile.name + ' ' + userData.profile.surname;
         //username
@@ -154,9 +188,49 @@ var makeProfileEditData = function (userData) {
     return deferred.promise;
 };
 
-function countAge(birthday) {
-    
-    return 20;
+function countAge(dateString) {
+    //http://stackoverflow.com/a/7091965
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
+
+function countLastLogin(dateString) {
+    if (!dateString) return 'Never logged in.'
+    var diff = Math.floor((Date.now() - (new Date(dateString)).getTime())/1000);
+    if (diff === 0) {
+        return 'Logged right now.';
+    }
+    else if(diff<60){ //seconds
+        return 'Logged '+ diff + 'seconds ago.';
+    }
+    else if(Math.floor(diff/60)<60){ //minutes
+        diff = Math.floor(diff/60);
+        return 'Logged ' + diff + 'minutes ago';
+    }
+    else if(Math.floor(diff/60)<24){
+        diff = Math.floor(diff/60);
+        return 'Logged ' + diff + 'hours ago';
+    }
+    else if(Math.floor(diff/24)<7){ //hours
+        diff = Math.floor(diff/24);
+        return 'Logged ' + Math.floor(diff/24) + 'days ago';
+    }
+    else if(diff<30){
+        return 'Logged ' + Math.floor(diff/7) + 'weeks ago';
+    }
+    else if(diff<365){
+        return 'Logged ' + Math.floor(diff/30) + 'months ago';
+    }
+    else {
+        return 'Logged ' + Math.floor(diff/365) + 'years ago';
+    }
+
 }
 
 module.exports=router;
