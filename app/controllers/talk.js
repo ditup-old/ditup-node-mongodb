ddedfc41d6065204b8fe7cb7c91c0d83ea64ebe2
mrkvon <michal.salajka@email.cz>
2015-06-10 06:18:03 +0200

    started messing with talk, changed header a bit

--- app/controllers/talk.js
+++ app/controllers/talk.js
@@ -4,11 +4,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-    res.render('login');
-});
-
-router.post('/', function (req, res, next) {
-    res.end(JSON.stringify(req.body));
+    res.render('talk', {session: {logged: req.session.logged, username: req.session.username}});
 });
 
 module.exports=router;
